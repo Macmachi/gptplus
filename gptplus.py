c29c56be2b0415389d7343c0ac46acfdf53473c4
Macmachi <38922703+Macmachi@users.noreply.github.com>
2023-05-31 22:32:26 +0200

    1.1.3
    Minor correction with the modification of asyncio.run() and addition of dependencies.

--- gptplus.py
+++ gptplus.py
@@ -481,6 +481,4 @@ async def main():
     await dp.start_polling()
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.close()
+    asyncio.run(main())
